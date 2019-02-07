5d1922f0ec6c825add360556936031ab0fa636de
samrose3 <samrose3@gmail.com>
2019-02-06 19:01:14 -0600

    Base line and index matching

--- static/src/app.js
+++ static/src/app.js
@@ -1,3 +1,103 @@
 import './styles.scss'
 
-console.log(`Let's get scripty`)
+class SearchApp {
+  constructor(opts = {}) {
+    // constants
+    this.prefix = opts.prefix || 'sa'
+    this.updateKeys = ['Enter', ' '];
+
+    // state
+    this.query = ''
+    this.input = ''
+    this.results = []
+
+    // DOM elements
+    this.$searchInput = document.querySelector(`.${this.prefix}-search-input`)
+    this.$textInput = document.querySelector(`.${this.prefix}-text-input`)
+    this.$fileInput = document.querySelector(`.${this.prefix}-file-input`)
+    this.$resultsList = document.querySelector(`.${this.prefix}-results-list`)
+
+    this.init()
+  }
+
+  init() {
+    this.$searchInput.addEventListener('keypress', this.onKeyPress.bind(this))
+    this.$textInput.addEventListener('keypress', this.onKeyPress.bind(this))
+    this.$fileInput.addEventListener('change', this.onFileChange.bind(this))
+  }
+
+  onKeyPress(e) {
+    if (this.updateKeys.indexOf(e.key) > -1) this.update()
+  }
+
+  async onFileChange(e) {
+    const [file] = e.currentTarget.files
+    const text = await this.parse(file)
+    this.$textInput.value = text || 'Can not read input file'
+    this.update()
+  }
+
+  async parse(file) {
+    const reader = new FileReader()
+    reader.readAsText(file)
+    const result = await new Promise((resolve, reject) => {
+      reader.onload = () => resolve(reader.result)
+    })
+    return result
+  }
+
+  update() {
+    this.query = this.$searchInput.value
+    this.input = this.$textInput.value.split('\n')
+    this.search()
+    this.render()
+  }
+
+  search() {
+    if (!this.query) this.results = []
+
+    this.results = this.input
+      .map(line => {
+        let match
+        const lineMatches = []
+        const re = new RegExp(`${this.query}`, 'igm')
+
+        while ((match = re.exec(line)) != null) {
+          console.log("match found at " + match.index);
+          lineMatches.push(match.index)
+        }
+
+        return lineMatches
+      })
+  }
+
+  render() {
+    const fragment = document.createDocumentFragment()
+
+    this.results.forEach((line, lineNumber) => {
+      console.log(line)
+      line.forEach(matchIdx => {
+        const li = document.createElement('li')
+        const start = matchIdx
+        const end = matchIdx + this.query.length
+        const matchText = this.input[lineNumber].slice(start, end)
+
+        const padding = 15
+        console.log((end + padding), this.input[lineNumber].length)
+        const hasEllipsis = (end + padding) < this.input[lineNumber].length
+
+        const contextText = this.input[lineNumber].slice(end, end + padding)
+        const html = `<span>${matchText}</span> ${contextText}${hasEllipsis ? ' ...' : ''}`
+        console.log(html)
+        li.innerHTML = html
+        fragment.appendChild(li)
+      })
+    })
+
+    this.$resultsList.innerHTML = ''
+    this.$resultsList.appendChild(fragment)
+  }
+}
+
+export default new SearchApp()
+
